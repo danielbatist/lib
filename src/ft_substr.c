8e0232c8ec35a6bf9d7a46426259a321f6d8eb9d
dbatista <dbs.batista05@gmail.com>
2024-10-09 00:11:26 -0300

    09/10

--- src/ft_substr.c
+++ src/ft_substr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dbatista <dbatista@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dbatista <dbatista@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 22:40:09 by dbatista          #+#    #+#             */
-/*   Updated: 2024/10/08 18:48:22 by dbatista         ###   ########.fr       */
+/*   Updated: 2024/10/09 00:08:38 by dbatista         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,24 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	unsigned char	*sub;
 	size_t			i;
+	size_t			s_len;
 
-	if (s == NULL || start >= ft_strlen(s))
+	s_len = ft_strlen(s);
+
+	if (!s)
 		return (NULL);
+	if (start >= s_len)
+	{
+		sub = malloc(1 * sizeof(char));
+		if (sub == NULL)
+			return (NULL);
+		sub[0] = '\0';
+		return ((char *)sub);
+	}
+	if (len > s_len - start)
+	{
+		len = s_len - start;
+	}
 	sub = malloc((len + 1) * sizeof(char));
 	if (sub == NULL)
 		return (NULL);
